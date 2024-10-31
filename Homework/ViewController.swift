00be16d0d0952f30dfa2b9e75880607b9fdcb234
Anatolij <vakulin.anatolij@bk.ru>
2024-10-31 10:08:06 +0300

    Button and main text

--- Homework/ViewController.swift
+++ Homework/ViewController.swift
@@ -12,12 +12,12 @@ final class ViewController: UIViewController {
     private let imageView = UIImageView()
     private let imageNotification = UIView()
     private let titleLabel = UILabel()
-    //private let maintext = UILabel()
-    //private let reviewButton = UIButton()
-    //private let laterButton = UIButton()
+    private let mainText = UILabel()
+    private let reviewButton = UIButton()
+    private let laterButton = UIButton()
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         embedViews()
@@ -29,11 +29,12 @@ final class ViewController: UIViewController {
     func embedViews() {
         view.addSubview(imageView)
         imageView.addSubview(imageNotification)
-        imageNotification.addSubview(titleLabel)
         
-        /*[
+        [
             titleLabel,
-        ].forEach { imageNotification.addSubview($0) }*/
+            mainText,
+            reviewButton,
+        ].forEach { imageNotification.addSubview($0) }
     }
     
     func setupLayout(){
@@ -48,8 +49,24 @@ final class ViewController: UIViewController {
             imageNotification.trailingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: -34),
             imageNotification.heightAnchor.constraint(equalToConstant: 402),
             
-            titleLabel.topAnchor.constraint(equalTo: imageView.topAnchor, constant: 320),
-            titleLabel.leadingAnchor.constraint(equalTo: imageNotification.leadingAnchor, constant: 24),
+            titleLabel.topAnchor.constraint(equalTo: imageNotification.topAnchor, constant: 40),
+            titleLabel.centerXAnchor.constraint(equalTo: imageNotification.centerXAnchor),
+            
+            mainText.topAnchor.constraint(equalTo: imageNotification.topAnchor, constant: 82),
+            mainText.leadingAnchor.constraint(equalTo: imageNotification.leadingAnchor, constant: 16),
+            mainText.trailingAnchor.constraint(equalTo: imageNotification.trailingAnchor, constant: -16),
+            
+            reviewButton.topAnchor.constraint(equalTo: mainText.bottomAnchor, constant: 36),
+            reviewButton.leadingAnchor.constraint(equalTo: imageNotification.leadingAnchor, constant: 16),
+            reviewButton.trailingAnchor.constraint(equalTo: imageNotification.trailingAnchor, constant: -16),
+            reviewButton.heightAnchor.constraint(equalToConstant: 54),
+            
+            laterButton.topAnchor.constraint(equalTo: reviewButton.topAnchor, constant: 78),
+            laterButton.leadingAnchor.constraint(equalTo: imageNotification.leadingAnchor, constant: 16),
+            laterButton.trailingAnchor.constraint(equalTo: imageNotification.trailingAnchor, constant: -16),
+            laterButton.heightAnchor.constraint(equalToConstant: 22),
+            
+            
         ])
     }
     
@@ -68,13 +85,32 @@ final class ViewController: UIViewController {
         imageNotification.translatesAutoresizingMaskIntoConstraints = false
         
         titleLabel.text = "Мы ценим ваше мнение!"
-        titleLabel.font = .systemFont(ofSize: 24, weight: .semibold)
+        titleLabel.font = .systemFont(ofSize: 24, weight: .regular)
         titleLabel.textColor = .black
         titleLabel.numberOfLines = 0
-        imageNotification.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainText.text = "Спасибо, что пользуетесь нашим приложением. Нам очень важно знать ваше мнение, чтобы продолжать улучшать качество и функциональность сервиса. Поделитесь своими впечатлениями или сообщите о проблемах, с которыми вы столкнулись. Ваши отзывы помогают нам становиться лучше каждый день!"
+        mainText.font = .systemFont(ofSize: 16, weight: .regular)
+        mainText.textColor = .black
+        mainText.textAlignment = .center
+        mainText.numberOfLines = 0
+        mainText.translatesAutoresizingMaskIntoConstraints = false
+        
+        reviewButton.setTitle("Оставить отзыв", for: .normal)
+        reviewButton.setTitleColor(.white, for: .normal)
+        reviewButton.backgroundColor = .blue
+        reviewButton.layer.cornerRadius = 20
+        reviewButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        laterButton.setTitle("Напомнить позже", for: .normal)
+        laterButton.setTitleColor(.blue, for: .normal)
+        laterButton.backgroundColor = .white
+        laterButton.translatesAutoresizingMaskIntoConstraints = false
+        
     }
     
     func setupBehavior() {}
-
 }
+
 
